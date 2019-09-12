1b6f73dc5899f39dfcd90e19ec0decbb6acc3557
Diego La Faye <lafayediego@gmail.com>
2019-09-12 12:03:39 -0400

    Cambiando clase employee

--- src/main/java/com/ejercicioSoftware/demo/EmployeeController.java
+++ src/main/java/com/ejercicioSoftware/demo/EmployeeController.java
@@ -1,7 +1,10 @@
 package com.ejercicioSoftware.demo;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,22 +13,28 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.hateoas.core.DummyInvocationUtils.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+
 @RestController
 class EmployeeController {
 
     private final EmployeeRepository repository;
+    private final EmployeeResourceAssembler assembler;
 
-    EmployeeController(EmployeeRepository repository) {
+    EmployeeController(EmployeeRepository repository,
+                       EmployeeResourceAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     // Aggregate root
-
+/*
     @GetMapping("/employees")
     List<Employee> all() {
         return repository.findAll();
     }
-
+*/
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
         return repository.save(newEmployee);
@@ -34,12 +43,28 @@ class EmployeeController {
     // Single item
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
+    Resource<Employee> one(@PathVariable Long id) {
 
-        return repository.findById(id)
+        Employee employee = repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+        return assembler.toResource(employee);
+        /*return new Resource<>(employee,
+                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    */
     }
 
+
+    @GetMapping("/employees")
+    Resources<Resource<Employee>> all() {
+
+        List<Resource<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+
+        return new Resources<>(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
